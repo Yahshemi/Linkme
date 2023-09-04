deffc156ed023787220bacc9ef74ebe05a7f1f0f
Yahshemi <yahshemi@gmail.com>
2023-09-03 17:50:37 -0700

    add linkedin and spotify

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,60 +1,67 @@
 import website from '../assets/website.png'
 import instagram from '../assets/instagram.png'
-import appstore from '../assets/playstore.png'
+import spotify from '../assets/spotify.png'
 import youtube from '../assets/youtube.jpeg'
-import dribbble from '../assets/dribbble.png'
+import flickr from '../assets/flickr.png'
 import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
 import twitter from '../assets/twitter.png'
+import linkedin from '../assets/linkedin.png'
 
 const items = [{
     "title": "Website",
     "subtitle": "Look at my work!",
     "image": website,
-    "link": "https://sagniksahoo.codes" //your personal website or portfolio  link
+    "link": "https://yahshemi.github.io/CSS-My-Site/" //your personal website or portfolio link
 },
 {
     "title": "GitHub",
-    "subtitle": "@heysagnik | 🏠 of my open-source projects",
+    "subtitle": "@yahshemi | 🏠 of my projects",
     "image": github,
-    "link": "https://github.com/heysagnik" //Github Profile link
+    "link": "https://github.com/Yahshemi" //Github Profile link
 },
 {
     "title": "Instagram",
-    "subtitle": "@heysagnik | Hub of my awesome 😎 pics",
+    "subtitle": "@yahshemi | Hub of my awesome 😎 pics",
     "image": instagram,
-    "link": "https://instagram.com/heysagnik" //instagram profile link 
+    "link": "https://instagram.com/yahshemi" //instagram profile link 
 },
 {
     "title": "Twitter",
-    "subtitle": "@heysagnik | Don't forget to follow me 😉",
+    "subtitle": "@yahshemi | Don't forget to follow me 😉",
     "image": twitter,
-    "link": "https://twitter.com/heysagnik"// twitter profile link 
+    "link": "https://twitter.com/yahshemi"// twitter profile link 
 },
 {
-    "title": "Apps",
-    "subtitle": "Hub of my awesome 🔥 Apps",
-    "image": appstore,
-    "link": "#"//
+    "title": "Spotify",
+    "subtitle": "Check out my music 🎵 ",
+    "image": spotify,
+    "link": "https://open.spotify.com/playlist/0FuLn00EawgjUXGw0NmN2V"//
 },
 {
     "title": "YouTube",
-    "subtitle": "CODER SAGNIK | Official channel of mine",
+    "subtitle": "Creating Coding Careers | Official channel of CCC",
     "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"//youtube channel link 
+    "link": "https://www.youtube.com/@cccareers"//youtube channel link 
 },
 
 {
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
+    "title": "Flickr",
+    "subtitle": "Shay Bezale-el | Hub to the Shots of my 😎 apps ",
+    "image": flickr,
+    "link": "https://www.flickr.com/photos/166823293@N04/" // Flickr profile link 
+},
+{
+    "title": "LinkedIn",
+    "subtitle": "Yahshemi - | My professional journey ",
+    "image": linkedin,
+    "link": "https://www.linkedin.com/in/yahshemi/" // LinkedIn profile link 
 },
 {
     "title": "Telegram",
-    "subtitle": "@heysagnik | Chat with me instantly, Don't hesitate! ",
+    "subtitle": "Yahshemi | Chat with me instantly, Don't hesitate! ",
     "image": telegram,
-    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
+    "link": "https://telegram.me/shay187" //Telegram Pofile 
 }]
 
 export default items
